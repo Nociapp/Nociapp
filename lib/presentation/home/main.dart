3a6ec5eb3472b0c1b22b250ad3703abfb8f1c407
Takaki Hamahiga <2tf2003@gmail.com>
2020-03-06 00:03:47 +0900

    ✨ Implemented BLoCBuilder in Home.

--- lib/presentation/home/main.dart
+++ lib/presentation/home/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nociapp/application/user/user_bloc.dart';
+import 'package:nociapp/application/user/user_event.dart';
+import 'package:nociapp/application/user/user_state.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}): super(key: key);
@@ -10,8 +14,31 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    // ignore: close_sinks
+    final UserBloc userBloc = BlocProvider.of<UserBloc>(context);
     return Scaffold(
-      body: Container()
+      body: BlocBuilder<UserBloc, UserState>(
+        bloc: userBloc,
+        builder: (BuildContext context, UserState state) {
+          if (state is Initial) {
+            userBloc.add(Request());
+            print("User requested.");
+          }
+          if (state is Loading) {
+            print("User loading.");
+          }
+          if (state is Completed) {
+            print("User loading completed.");
+          }
+          // Return main page after User loading completed.
+          if (state is UserStream) {
+            return Scaffold(
+              body: Container()
+            );
+          }
+          return Container();
+        },
+      )
     );
   }
 }
