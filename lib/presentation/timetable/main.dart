6aa6168c3dc9524c3e386a66d9cfe6b0690bf887
Takaki Hamahiga <2tf2003@gmail.com>
2020-03-19 04:37:15 +0900

    ✨ Timetable

---
+++ lib/presentation/timetable/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Timetable extends StatefulWidget {
+  @override
+  _Timetable createState() => _Timetable();
+}
+
+class _Timetable extends State<Timetable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
