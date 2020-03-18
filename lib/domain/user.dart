9c12fbbdf7f775634ecc0af6e6e8517f12228bf7
Takaki Hamahiga <2tf2003@gmail.com>
2020-03-19 05:20:39 +0900

    ✨ Create User.

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -1,3 +1,8 @@
-class User {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
+class User {
+  final FirebaseUser firebaseUser;
+
+  User({@required this.firebaseUser}): assert(firebaseUser != null);
 }
--- lib/infrastructure/repository/impl_user_repository.dart
+++ lib/infrastructure/repository/impl_user_repository.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nociapp/domain/repository/user_repository.dart';
 import 'package:nociapp/domain/user.dart';
 
 class UserRepository implements IUserRepository {
   @override
   Future<User> user() async{
-    return User();
+    var auth = FirebaseAuth.instance;
+    var firebaseUser = await auth.currentUser();
+    return User(firebaseUser: firebaseUser);
   }
 }
