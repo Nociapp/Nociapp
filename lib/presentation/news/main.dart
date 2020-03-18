427a84413519d435f74d17461e3e43d93f77f881
Takaki Hamahiga <2tf2003@gmail.com>
2020-03-19 04:36:56 +0900

    ✨ News

---
+++ lib/presentation/news/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class News extends StatefulWidget {
+  @override
+  _News createState() => _News();
+}
+
+class _News extends State<News> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.teal);
+  }
+}
